1ca7692e1bd484f4a91140dc1ba32cbe4d37e3e0
sudopapa <112443607+sudopapa@users.noreply.github.com>
2022-10-30 22:34:49 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -21,9 +21,14 @@ function injectHTML(list) {
   console.log('fired injectHTML');
   const target = document.querySelector("#restaurant_list");
   target.innerHTML='';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
   list.forEach(item => {
-    const str = `<li>${item.name} </li>`;
-    target.innerHTML += str;
+    const el = document.createElement('li');
+    el.innerText = item.name;
+    listEl.appendChild(el);
+    
   })
 
 
