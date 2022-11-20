5f52dfd3710c8b1dbfae795c42fce30e00d71dd2
sudopapa <112443607+sudopapa@users.noreply.github.com>
2022-11-20 14:00:29 -0500

    Lab 7 Complete

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -90,6 +90,16 @@ function getRandomIntInclusive(min, max) {
   
   
   }
+
+  function filterList(array, filterInputValue) {
+    return array.filter((item) => {
+      if (!item.name) { return; }
+      const lowerCaseName = item.name.toLowerCase();
+      const lowerCaseQuery = filterInputValue.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    })
+    return newArray;
+  }
   
   async function mainEvent() {
     /*
@@ -119,7 +129,8 @@ function getRandomIntInclusive(min, max) {
       Dot notation is preferred in JS unless you have a good reason to use brackets
       The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
     */
-    //console.table(arrayFromJson.data);
+    console.log('Test PG County Set')
+    console.table(arrayFromJson.data);
   
     // in your browser console, try expanding this object to see what fields are available to work with
     // for example: arrayFromJson.data[0].name, etc
@@ -129,15 +140,23 @@ function getRandomIntInclusive(min, max) {
     console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
   
     // This IF statement ensures we can't do anything if we don't have information yet
-    if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+    if (arrayFromJson.data?.length > 0) {  
+    
+      // the question mark in this means "if this is set at all"
       submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
   
       // Let's hide the load button now that we have some data to manipulate
       loadAnimation.classList.remove('lds-ellipsis');
       loadAnimation.classList.add('lds-ellipsis_hidden');
 
+      let currentList = [];
 
-  
+      form.addEventListener('input', (event)=> {
+        console.log('input', event.target.value);
+        const filteredList = filterList(currentList, event.target.value);
+        injectHTML(filteredList);
+      });
+
       // And here's an eventListener! It's listening for a "submit" button specifically being clicked
       // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
       form.addEventListener('submit', (submitEvent) => {
@@ -145,11 +164,11 @@ function getRandomIntInclusive(min, max) {
         submitEvent.preventDefault();
   
         // This constant will have the value of your 15-restaurant collection when it processes
-        const restaurantList = processRestaurants(arrayFromJson.data);
-        console.log(restaurantList);
+        currentList = processRestaurants(arrayFromJson.data);
+        console.log(currentList);
   
         // And this function call will perform the "side effect" of injecting the HTML list for you
-        injectHTML(restaurantList);
+        injectHTML(currentList);
   
         // By separating the functions, we open the possibility of regenerating the list
         // without having to retrieve fresh data every time
