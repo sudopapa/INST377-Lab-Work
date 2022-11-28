6c53bf3eee40147aa8e9d75df0e3c625b64fc1ad
sudopapa <112443607+sudopapa@users.noreply.github.com>
2022-11-27 22:52:20 -0500

    Final Map Update

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -111,17 +111,18 @@ function getRandomIntInclusive(min, max) {
   }
 
   function markerPlace(array, map) {
-    // const marker = L.marker([51.5, -0.09]).addTo(map);
     map.eachLayer((layer) => {
         if (layer instanceof L.Marker) {
           layer.remove();
         }
     });
 
-    array.forEach(item => {
+    array.forEach((item, index) => {
         const {coordinates} = item.geocoded_column_1;
         L.marker([coordinates[1], coordinates[0]]).addTo(map);
-
+        if (index ===  0) {
+            map.setView([coordinates[1], coordinates[0]], 10);
+        }
     });
   }
 
@@ -181,7 +182,7 @@ function getRandomIntInclusive(min, max) {
         console.log('input', event.target.value);
         const filteredList = filterList(currentList, event.target.value);
         injectHTML(filteredList);
-        markerPlace(currentList, pageMap);
+        markerPlace(filteredList, pageMap);
       });
 
       // And here's an eventListener! It's listening for a "submit" button specifically being clicked
